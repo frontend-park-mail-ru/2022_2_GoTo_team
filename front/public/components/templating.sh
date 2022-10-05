08cbc6d7d1dc126d67aea981b9473be3f26101cb
FackOff25 <kozvladimir@bk.ru>
2022-10-05 16:21:43 +0300

    scropts

--- front/public/components/templating.sh
+++
@@ -1,4 +0,0 @@
-handlebars html/navbar.html -f navbar.tmpl.js
-handlebars html/article.html -f article.tmpl.js
-handlebars html/login_form.html -f login_form.tmpl.js
-handlebars html/registration_form.html -f registration_form.tmpl.js
---
+++ front/templating.sh
@@ -0,0 +1,5 @@
+mkdir front/public/components/tmpl
+handlebars front/public/components/html/navbar.html -f front/public/components/tmpl/navbar.tmpl.js
+handlebars front/public/components/html/article.html -f front/public/components/tmpl/article.tmpl.js
+handlebars front/public/components/html/login_form.html -f front/public/components/tmpl/login_form.tmpl.js
+handlebars front/public/components/html/registration_form.html -f front/public/components/tmpl/registration_form.tmpl.js
