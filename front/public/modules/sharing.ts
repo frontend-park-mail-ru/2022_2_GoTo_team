933e87107a04e564f3b43f9273ac5b8fe97a6fce
FackOff25 <kozvladimir@bk.ru>
2022-12-22 15:41:32 +0300

    basic open graph

--- front/public/modules/sharing.ts
+++ front/public/modules/sharing.ts
@@ -19,7 +19,7 @@ export class OpenGraphHelper{
         } else {
             OpenGraphHelper.#updateMetaTag(meta, value);
         }
-
+        /*
         value = data.title;
         meta = document.querySelector('meta[property="og:title"]');
         if (meta === null) {
@@ -27,7 +27,7 @@ export class OpenGraphHelper{
         } else {
             OpenGraphHelper.#updateMetaTag(meta, value);
         }
-
+        */
         value = data.description;
         if (value !== undefined) {
             meta = document.querySelector('meta[property="og:description"]');
